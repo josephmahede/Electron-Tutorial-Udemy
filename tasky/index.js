834c5c4e1ffc6f56171e3edf23e1b9440ae9c8ed
Joseph Ma <dev@mahedejoseph.com>
2022-02-18 02:16:05 -0800

    Create MainWindow class, refactor index.js

--- tasky/index.js
+++ tasky/index.js
@@ -1,21 +1,15 @@
 const electron = require('electron');
 const path = require('path');
-const { app, BrowserWindow, Tray } = electron;
+const MainWindow = require('./app/main_window');
 const TimerTray = require('./app/timer_tray');
+
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 let tray
 
 app.on('ready', () => {
-    
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false,
-        skipTaskBar: true
-    });
+    mainWindow = new MainWindow(`file://${__dirname}\\src\\index.html`);
 
     if (process.platform == 'darwin') {
         app.dock.hide();
@@ -23,10 +17,8 @@ app.on('ready', () => {
         mainWindow.setSkipTaskbar(true);
     }
 
-    mainWindow.loadURL(`file://${__dirname}\\src\\index.html`);
-    mainWindow.on('blur', () => {
-        mainWindow.hide();
-    });
+    // mainWindow.loadURL(`file://${__dirname}\\src\\index.html`);
+    
 
     const iconName = process.platform ==='win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
